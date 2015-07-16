e540c04f9759f9a38181ff9167fd3e02e2ea0788
Florian MARGAINE <florian@margaine.com>
2015-07-16 03:12:07 +0200

    Start dogfooding

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
-SOURCES := $(wildcard src/*.lisp) $(wildcard *.asd)
+SOURCES := $(wildcard src/*.lisp) $(wildcard *.asd) $(wildcard t/*.lisp)
+
+.PHONY: dist
 
 deb-packager: quicklisp-manifest.txt $(SOURCES)
 	@buildapp  --manifest-file quicklisp-manifest.txt \
@@ -13,3 +15,11 @@ quicklisp-manifest.txt:
 		--eval '(push #P"$(ROOT_DIR)/" asdf:*central-registry*)' \
 		--eval '(ql:quickload :deb-packager)' \
 		--eval '(ql:write-asdf-manifest-file "quicklisp-manifest.txt")'
+
+dist:
+	@make deb-packager
+	@mkdir -p dist/
+	@cp deb-packager dist/
+	@./deb-packager dist/deb-packager.lisp
+	@mv deb-packager_*_amd64.deb dist/
+	@rm -f dist/deb-packager
---
+++ dist/deb-packager.lisp
@@ -0,0 +1,18 @@
+(define-deb-package deb-packager
+  (:architecture "amd64")
+  (:description "Simply create a debian package by defining an s-expression.")
+  (:long-description "Defining a debian package by using devscripts is relatively
+painful. A list of files have to be created to be able to create the
+debian package we want. deb-packager's aim is to solve this issue, and
+make it painless to create a debian package. It also aims to be able to
+reproduce everything dpkg-buildpackage can do.")
+  (:maintainer "Florian Margaine <florian@margaine.com>")
+  (:changelog
+   (:version "1.0"
+    :author "Florian Margaine <florian@margaine.com>"
+    :message "Initial release."
+    :date 1437004038))
+  (:data-files
+   (:path #p"usr/bin/deb-packager"
+    :mode 755
+    :content (alexandria:read-file-into-byte-vector "dist/deb-packager"))))
